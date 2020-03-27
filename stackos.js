bcda57ece9c58be26e6ce2a367d7896d74a1da17
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-03-27 20:59:25 +0100

    Change OS Color

--- stackos.js
+++ stackos.js
@@ -62,7 +62,7 @@ module.exports = {
 
     // Chalk strings
     const out = {
-      os: chalk.cyan(this.info.os),
+      os: chalk.green(this.info.os),
       arch: chalk.grey(this.info.arch),
       cores: `${cores} Core`,
       cpu: chalk.grey(this.info.cpus.model),
